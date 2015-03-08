d512667b03ac23fb7d274de7662fedd8423d569e
Will Slotkin <william.slotkin@gmail.com>
2015-03-07 22:56:09 -0600

    Initial Commit

---
+++ cta_tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cta_tracker
+//
+//  Created by Will Slotkin on 3/7/15.
+//  Copyright (c) 2015 3005 Productions Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
